9d9993d1d5e05245fa395a47958dc33a6057d9af
MadupuHiranya07 <madupuhiranya07@gmail.com>
2025-04-23 14:49:59 +0530

    new

---
+++ q28.js
@@ -0,0 +1,71 @@
+db.students.insertOne({
+    studentName:"John"
+});
+
+db.subjects.insertOne({
+    subjectName:"Maths"
+});
+
+db.marks.insertOne({
+    studentId:ObjectId('6808a74dd21aa8b820b5f8a5'),
+    subjectId:ObjectId('6808acbfd21aa8b820b5f8a8'),
+    score:95
+});
+db.marks.find({},{_id:0,studentId:1,subjectId:1,score:1});
+
+db.marks.find({},{_id:0,score:1});
+
+db.marks.aggregate([
+    {$lookup:{
+        from:"subjects",
+        localField:"subjectId",
+        foreignField:"_id",
+        as:"subjects",
+    },
+},
+{
+    $lookup:{
+        from:"students",
+        localField:"studentId",
+        foreignField:"_id",
+        as:"students",
+    }
+},
+{$unwind:"$students"},
+{$project:{
+    _id:0,
+    "students.studentName":1,
+    "subjects.subjectName":1,
+    score:1,
+}}
+]);
+
+
+db.createView("marksView","marks",[
+    {$lookup:{
+        from:"subjects",
+        localField:"subjectId",
+        foreignField:"_id",
+        as:"subjects",
+    },
+},
+{
+    $lookup:{
+        from:"students",
+        localField:"studentId",
+        foreignField:"_id",
+        as:"students",
+    }
+},
+{$unwind:"$students"},
+{$project:{
+    // _id:0,
+    "students.studentName":1,
+    "subjects.subjectName":1,
+    score:1,
+}}
+]);
+
+
+db.marksView.find()
+db.marksView.drop()
